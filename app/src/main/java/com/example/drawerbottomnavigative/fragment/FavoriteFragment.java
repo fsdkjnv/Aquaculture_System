92b7738bcb164e53a857e21c07ae415490905892
vothithuyvi <vovilak10202002@gmail.com>
2023-12-19 14:22:19 +0700

    Vi vommit

--- app/src/main/java/com/example/drawerbottomnavigative/fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/drawerbottomnavigative/fragment/FavoriteFragment.java
@@ -1,34 +1,37 @@
+
 package com.example.drawerbottomnavigative.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.example.drawerbottomnavigative.MainActivity;
 import com.example.drawerbottomnavigative.MqttHandler;
 import com.example.drawerbottomnavigative.R;
+import android.os.Handler;
+
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class FavoriteFragment extends Fragment {
-
+    public double ph;
+    private double temperature;
+    private double turbidity;
+    private String status;
     private static final String CLIENT_ID = "vovilak10202002";
     private static final String OXYGEN_PUMP_TOPIC = "control-oxygen-pump";
-    private static final String  WATER_PUMP_TOPIC = "control-water-pump";
-     private static final String RESTORE_TOPIC = "control-restore";
-    private MqttHandler mqttHandler;
+    private static final String WATER_PUMP_TOPIC = "control-water-pump";
+    private static final String RESTORE_TOPIC = "control-restore";
     private Switch switchOxy;
     private Switch switchWater;
-
-    // Flags to prevent recursive calls during switch state changes
-    private boolean oxySwitchChanging;
-    private boolean waterSwitchChanging;
+    private MqttViewModel viewModel;
+    MqttHandler mqttHandler;
 
     @Nullable
     @Override
@@ -36,6 +39,10 @@ public class FavoriteFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_favorite, container, false);
         switchOxy = view.findViewById(R.id.switchoxy);
         switchWater = view.findViewById(R.id.switchwater);
+        // Khởi tạo ViewModel
+        viewModel = new ViewModelProvider(requireActivity()).get(MqttViewModel.class);
+        setupMqttConnectionAndSubscribe();
+
 
         // Set up listeners for user-initiated changes to the switches
         setupSwitchListeners();
@@ -47,76 +54,177 @@ public class FavoriteFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Lấy thông tin từ ViewModel
+        viewModel.getOxygenSwitchState().observe(getViewLifecycleOwner(), isChecked -> switchOxy.setChecked(isChecked));
+        viewModel.getWaterSwitchState().observe(getViewLifecycleOwner(), isChecked -> switchWater.setChecked(isChecked));
 
-        mqttHandler = new MqttHandler();
-        mqttHandler.connectToHiveMQ(CLIENT_ID);
+        // Khởi tạo hoặc lấy lại kết nối MQTT từ ViewModel
+         mqttHandler = viewModel.getMqttHandler();
 
-        // Subscribe to MQTT topics
-        subscribeToSwitch(WATER_PUMP_TOPIC);
-        subscribeToSwitch(OXYGEN_PUMP_TOPIC);
-        publishMessage(RESTORE_TOPIC, "ENABLE");
 
+        if (mqttHandler == null) {
+            mqttHandler = new MqttHandler();
+            mqttHandler.connectToHiveMQ(CLIENT_ID);
+            viewModel.setMqttHandler(mqttHandler);
+            publishMessage(RESTORE_TOPIC, "ENABLE");
+        }
     }
 
     @Override
     public void onDestroyView() {
-        mqttHandler.disconnect();
+        // Hủy đăng ký lắng nghe khi Fragment bị hủy
+        viewModel.getOxygenSwitchState().removeObservers(getViewLifecycleOwner());
+        viewModel.getWaterSwitchState().removeObservers(getViewLifecycleOwner());
+
         super.onDestroyView();
     }
 
     private void setupSwitchListeners() {
         // Set up listener for the Oxygen Pump switch
-        publishMessageOnClick(switchOxy, OXYGEN_PUMP_TOPIC);
+        switchOxy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle the state of the switch
+                // Publish the first message based on the new state
+                publishMessage(OXYGEN_PUMP_TOPIC, switchOxy.isChecked() ? "ON" : "OFF");
+
+                // Delay for 1 second and then publish the second message
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishMessage(OXYGEN_PUMP_TOPIC, switchOxy.isChecked() ? "ON" : "OFF");
+
+                        // Delay for 1 second and then publish the third message
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                publishMessage(OXYGEN_PUMP_TOPIC, switchOxy.isChecked() ? "ON" : "OFF");
+                            }
+                        }, 1000); // 1000 milliseconds = 1 second
+                    }
+                }, 1000); // 1000 milliseconds = 1 second
+            }
+        });
 
         // Set up listener for the Water Pump switch
-        publishMessageOnClick(switchWater, WATER_PUMP_TOPIC);
-    }
-
-private void publishMessageOnClick(Switch aSwitch, String topic) {
-    aSwitch.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            boolean isChecked = ((Switch) view).isChecked();
-
-            if (!oxySwitchChanging && !waterSwitchChanging) {
-                // Only publish if the change is user-initiated
-                String message = isChecked ? "ON" : "OFF";
-                publishMessage(topic, message);
-            }
-        }
-    });
-}
-
-
-    private void subscribeToSwitch(String topic) {
-        mqttHandler.subscribe(topic, new MqttHandler.MessageCallback() {
+        switchWater.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMessageReceived(String receivedTopic, String message) {
-                boolean isChecked = "ON".equals(message);
-                switchChanging(receivedTopic, isChecked);
-            }
+            public void onClick(View v) {
+                // Toggle the state of the switch
+                // Publish the first message based on the new state
+                publishMessage(WATER_PUMP_TOPIC, switchWater.isChecked() ? "ON" : "OFF");
 
-            @Override
-            public void onConnectionLost(Throwable cause) {
-                // Handle connection lost event
+                // Delay for 1 second and then publish the second message
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishMessage(WATER_PUMP_TOPIC, switchWater.isChecked() ? "ON" : "OFF");
+
+                        // Delay for 1 second and then publish the third message
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                publishMessage(WATER_PUMP_TOPIC, switchWater.isChecked() ? "ON" : "OFF");
+                            }
+                        }, 1000); // 1000 milliseconds = 1 second
+                    }
+                }, 1000); // 1000 milliseconds = 1 second
             }
         });
     }
 
-    private void switchChanging(String receivedTopic, boolean isChecked) {
-        if (WATER_PUMP_TOPIC.equals(receivedTopic)) {
-            waterSwitchChanging = true;
-            requireActivity().runOnUiThread(() -> switchWater.setChecked(isChecked));
-            waterSwitchChanging = false;
-        } else if (OXYGEN_PUMP_TOPIC.equals(receivedTopic)) {
-            oxySwitchChanging = true;
-            requireActivity().runOnUiThread(() -> switchOxy.setChecked(isChecked));
-            oxySwitchChanging = false;
-        }
-    }
+
 
     private void publishMessage(String topic, String message) {
         Toast.makeText(requireContext(), "Publishing message: " + message, Toast.LENGTH_SHORT).show();
-        mqttHandler.publish(topic, message);
+        viewModel.getMqttHandler().publish(topic, message);
+    }
+
+    private void setupMqttConnectionAndSubscribe() {
+    mqttHandler = viewModel.getMqttHandler();
+
+    if (mqttHandler == null) {
+        mqttHandler = new MqttHandler();
+        viewModel.setMqttHandler(mqttHandler);
+        mqttHandler.connectToHiveMQ(CLIENT_ID);
+    }
+
+    subscribeToTopic("collection-station");
+    subscribeToTopic(OXYGEN_PUMP_TOPIC);
+    subscribeToTopic(WATER_PUMP_TOPIC);
+}
+
+    private void subscribeToTopic(String topic) {
+        mqttHandler.subscribe(topic, new MqttHandler.MessageCallback() {
+            @Override
+            public void onMessageReceived(String receivedTopic, String message) {
+                processMqttMessage(message);
+
+                if (OXYGEN_PUMP_TOPIC.equals(receivedTopic)) {
+                    boolean isChecked = "ON".equals(message);
+                    viewModel.getOxygenSwitchState().postValue(isChecked);
+                    Log.e("oxy", isChecked + "");
+                } else if (WATER_PUMP_TOPIC.equals(receivedTopic)) {
+                    boolean isChecked = "ON".equals(message);
+                    viewModel.getWaterSwitchState().postValue(isChecked);
+                    Log.e("oxy", isChecked + "");
+                }
+            }
+
+            @Override
+            public void onConnectionLost(Throwable cause) {
+                handleMqttConnectionLost();
+            }
+        });
+    }
+
+
+    private void processMqttMessage(String message) {
+        String[] values = message.split(",");
+        if (values.length >= 4) {
+            ph = parseDouble(values[0]);
+            temperature = parseDouble(values[1]);
+            turbidity = parseDouble(values[2]);
+            status = getStatus(values[3]);
+          //  viewModel.updateValues(ph, temperature, turbidity, status);
+
+            // Log the variables
+            Log.d("TAG1", "pH: " + ph);
+            Log.d("TAG2", "Temperature: " + temperature);
+            Log.d("TAG3", "Turbidity: " + turbidity);
+            Log.d("TAG4", "Status: " + status);
+
+            updateUI();
+        }
+    }
+
+    private double parseDouble(String value) {
+        return NumberUtils.isCreatable(value) ? Double.parseDouble(value) : Double.NaN;
+    }
+
+    private String getStatus(String value) {
+        return value.equals("Stable") ? "Stable" : value.equals("Unstable") ? "Unstable" : "N/A";
+    }
+
+    private void handleMqttConnectionLost() {
+        Log.e("TAG", "Connection lost");
+
+        // Reconnect to MQTT
+        viewModel.setMqttHandler(mqttHandler);
+        mqttHandler.connectToHiveMQ(CLIENT_ID);
+
+        // Observe LiveData and update UI after reconnection
+
+        // Update UI after reconnection
+        updateUI();
+
+        // Subscribe again after reconnecting
+        setupMqttConnectionAndSubscribe();
+    }
+    private void updateUI() {
+        requireActivity().runOnUiThread(() -> {
+            viewModel.updateAllValues(ph, temperature, turbidity, status, "");
+
+        });
     }
 }
--- app/src/main/java/com/example/drawerbottomnavigative/fragment/SlideshowFragment.java
+++ app/src/main/java/com/example/drawerbottomnavigative/fragment/SlideshowFragment.java
@@ -5,7 +5,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.drawerbottomnavigative.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -25,7 +31,9 @@ import java.util.List;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class SlideshowFragment extends Fragment {
-
+    private static final String OXYGEN_PUMP_TOPIC = "control-oxygen-pump";
+    private static final String WATER_PUMP_TOPIC = "control-water-pump";
+    private static final String RESTORE_TOPIC = "control-restore";
     private LineChart lineChart1;
     private LineChart lineChart2;
     private LineChart lineChart3;
@@ -34,121 +42,90 @@ public class SlideshowFragment extends Fragment {
     private double ph = Double.NaN;
     private double temperature = Double.NaN;
     private double turbidity = Double.NaN;
+    private MqttViewModel viewModel ;
+    private String status;
 
     private float[] temperatureValues = new float[100];  // Adjust the size based on your needs
     private float[] pondusHydrogeniiValues = new float[100];
     private float[] turbidityValues = new float[100];
     private String[] timestamps = new String[100];  // Adjust the size based on your needs
     private int index = 0;
+     private List<Double> latestPhValues = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sildeshow, container, false);
-        // Initialize mqttHandler and connect to HiveMQ
-        mqttHandler = new MqttHandler();
-        mqttHandler.connectToHiveMQ(CLIENT_ID);
-        // Subscribe to MQTT topics and update switches when messages are received
-        subscribe();
+
         // Initialize LineCharts
+
         lineChart1 = view.findViewById(R.id.chartViewTemp1);
         lineChart2 = view.findViewById(R.id.chartViewTemp2);
         lineChart3 = view.findViewById(R.id.chartViewTemp3);
-
         // Customize the appearance of the LineCharts
+
         customizeLineChart(lineChart1, "Time", "Value");
         customizeLineChart(lineChart2, "Time", "Value");
         customizeLineChart(lineChart3, "Time", "Value");
 
+        viewModel = new ViewModelProvider(requireActivity()).get(MqttViewModel.class);
+
+        // Hàm kiểm tra xem tất cả các giá trị trong mảng có đều bằng 0 hay không
+
+
         return view;
     }
 
-    private void customizeLineChart(LineChart chart, String xAxisLabel, String yAxisLabel) {
-        // Hide right YAxis
-        chart.getAxisRight().setEnabled(false);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupMqttConnectionAndSubscribe();
 
-        // Customize XAxis (x-axis labels)
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setTextColor(ColorTemplate.getHoloBlue());
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(timestamps)); // Use timestamps as X-axis labels
 
-        // Customize YAxis (y-axis labels)
-        YAxis leftYAxis = chart.getAxisLeft();
-        leftYAxis.setTextColor(ColorTemplate.getHoloBlue());
-
-        // Customize Legend (dataset labels)
-        Legend legend = chart.getLegend();
-        legend.setTextColor(ColorTemplate.getHoloBlue());
     }
 
-    private void subscribe() {
-        mqttHandler.subscribe("collection-station", new MqttHandler.MessageCallback() {
-    @Override
-    public void onMessageReceived(String receivedTopic, String message) {
-        if ("collection-station".equals(receivedTopic)) {
-            if (message != null) {
-                String Mess = message;
+    private void setupMqttConnectionAndSubscribe() {
+    mqttHandler = viewModel.getMqttHandler();
 
-                String[] values = Mess.split(",");
-                if (values.length >= 4) {
-                    ph = NumberUtils.isCreatable(values[0]) ? Double.parseDouble(values[0]) : Double.NaN;
-                    temperature = NumberUtils.isCreatable(values[1]) ? Double.parseDouble(values[1]) : Double.NaN;
-                    turbidity = NumberUtils.isCreatable(values[2]) ? Double.parseDouble(values[2]) : Double.NaN;
-                    long currentTime = System.currentTimeMillis();
+    if (mqttHandler == null) {
+        mqttHandler = new MqttHandler();
+        viewModel.setMqttHandler(mqttHandler);
+        mqttHandler.connectToHiveMQ(CLIENT_ID);
+    }
 
-                    // Log timestamp
-                    logTimestamp(currentTime);
+    subscribeToTopic("collection-station");
+    subscribeToTopic(OXYGEN_PUMP_TOPIC);
+    subscribeToTopic(WATER_PUMP_TOPIC);
+}
 
-                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-                    String formattedDate = sdf.format(new Date(currentTime));
-                    Log.d("TAG_TIME", "Timestamp: " + formattedDate);
-                    Log.d("TAG1  " + formattedDate, "pH: " + ph);
-                    Log.d("TAG2", "Temperature: " + temperature);
-                    Log.d("TAG3", "Turbidity: " + turbidity);
-                    // Update chart entries with new data
-                    addChartData(currentTime, temperature, ph, turbidity);
+    private void subscribeToTopic(String topic) {
+        mqttHandler.subscribe(topic, new MqttHandler.MessageCallback() {
+            @Override
+            public void onMessageReceived(String receivedTopic, String message) {
+                processMqttMessage(message);
 
-                    // Notify charts to update
-                    updateCharts();
+                if (OXYGEN_PUMP_TOPIC.equals(receivedTopic)) {
+                    boolean isChecked = "ON".equals(message);
+                   viewModel.getOxygenSwitchState().postValue(isChecked);
+                    Log.e("oxy", isChecked + "");
+                } else if (WATER_PUMP_TOPIC.equals(receivedTopic)) {
+                    boolean isChecked = "ON".equals(message);
+                    viewModel.getWaterSwitchState().postValue(isChecked);
+                    Log.e("oxy", isChecked + "");
                 }
             }
-        }
-    }
 
             @Override
             public void onConnectionLost(Throwable cause) {
-                // Handle the event of a lost connection
+                handleMqttConnectionLost();
             }
-
         });
     }
 
-    private void logTimestamp(long timestamp) {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        String formattedDate = sdf.format(new Date(timestamp));
-        Log.d("TAG_TIME", "Timestamp: " + formattedDate);
-    }
-
-    private void addChartData(long timestamp, double temperature, double ph, double turbidity) {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        String formattedDate = sdf.format(new Date(timestamp));
-
-        temperatureValues[index % 5] = (float) temperature;
-        pondusHydrogeniiValues[index % 5] = (float) ph;
-        turbidityValues[index % 5] = (float) turbidity;
-        timestamps[index % 5] = formattedDate;
-
-        index++;
-
-        // Update chart data with the new LineDataSets
-        updateCharts();
-    }
-
     private void updateCharts() {
-        // Update the LineDataSets with the new entries
-        LineDataSet dataSet1 = new LineDataSet(getEntries(temperatureValues), "Temperature");
-        LineDataSet dataSet2 = new LineDataSet(getEntries(pondusHydrogeniiValues), "Pondus Hydrogenii");
-        LineDataSet dataSet3 = new LineDataSet(getEntries(turbidityValues), "Turbidity of Water");
+        // Update the LineDataSets with the saved entries
+        LineDataSet dataSet1 = new LineDataSet(getEntries(temperatureValues, index), "Temperature");
+        LineDataSet dataSet2 = new LineDataSet(getEntries(pondusHydrogeniiValues, index), "Pondus Hydrogenii");
+        LineDataSet dataSet3 = new LineDataSet(getEntries(turbidityValues, index), "Turbidity of Water");
 
         // Create LineData objects
         LineData lineData1 = new LineData(dataSet1);
@@ -165,12 +142,128 @@ public class SlideshowFragment extends Fragment {
         lineChart2.invalidate();
         lineChart3.invalidate();
     }
+    private void processMqttMessage(String message) {
+        String[] values = message.split(",");
+        if (values.length >= 4) {
+            ph = parseDouble(values[0]);
+            temperature = parseDouble(values[1]);
+            turbidity = parseDouble(values[2]);
+            status = getStatus(values[3]);
+          //  viewModel.updateValues(ph, temperature, turbidity, status);
+            long currentTime = System.currentTimeMillis();
 
-    private List<Entry> getEntries(float[] values) {
+                    // Log timestamp
+           logTimestamp(currentTime);
+
+           SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+           String formattedDate = sdf.format(new Date(currentTime));
+            // Log the variables
+            Log.d("TAG1", "pH: " + ph);
+            Log.d("TAG2", "Temperature: " + temperature);
+            Log.d("TAG3", "Turbidity: " + turbidity);
+            Log.d("TAG4", "Status: " + status);
+            Log.d("TAG_TIME", "Timestamp: " + formattedDate);
+            Log.d("TAG1  " + formattedDate, "pH: " + ph);
+            Log.d("TAG2", "Temperature: " + temperature);
+            Log.d("TAG3", "Turbidity: " + turbidity);
+            addChartData(currentTime, temperature, ph, turbidity);
+            updateCharts();
+            updateUI();
+        }
+    }
+
+    private double parseDouble(String value) {
+        return NumberUtils.isCreatable(value) ? Double.parseDouble(value) : Double.NaN;
+    }
+
+    private String getStatus(String value) {
+        return value.equals("Stable") ? "Stable" : value.equals("Unstable") ? "Unstable" : "N/A";
+    }
+
+    private void handleMqttConnectionLost() {
+        Log.e("TAG", "Connection lost");
+
+        // Reconnect to MQTT
+        viewModel.setMqttHandler(mqttHandler);
+        mqttHandler.connectToHiveMQ(CLIENT_ID);
+
+        // Observe LiveData and update UI after reconnection
+
+        // Update UI after reconnection
+        updateUI();
+
+        // Subscribe again after reconnecting
+        setupMqttConnectionAndSubscribe();
+    }
+    private void updateUI() {
+        requireActivity().runOnUiThread(() -> {
+            viewModel.updateAllValues(ph, temperature, turbidity, status, "");
+        });
+    }
+
+    private void customizeLineChart(LineChart chart, String xAxisLabel, String yAxisLabel) {
+        // Hide right YAxis
+        chart.getAxisRight().setEnabled(false);
+
+        // Customize XAxis (x-axis labels)
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextColor(ColorTemplate.getHoloBlue());
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(timestamps)); // Use timestamps as X-axis labels
+        xAxis.setTextSize(13);
+        chart.setVisibleXRangeMaximum(5); // Show only 5 entries at a time
+
+        // Customize YAxis (y-axis labels)
+        YAxis leftYAxis = chart.getAxisLeft();
+        leftYAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftYAxis.setTextSize(13);
+
+        // Customize Legend (dataset labels)
+        Legend legend = chart.getLegend();
+        legend.setTextColor(ColorTemplate.getHoloBlue());
+    }
+
+
+
+    private void logTimestamp(long timestamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formattedDate = sdf.format(new Date(timestamp));
+        Log.d("TAG_TIME", "Timestamp: " + formattedDate);
+    }
+
+
+    private void addChartData(long timestamp, double temperature, double ph, double turbidity) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formattedDate = sdf.format(new Date(timestamp));
+
+        if (index < temperatureValues.length && index < pondusHydrogeniiValues.length
+                && index < turbidityValues.length && index < timestamps.length) {
+            temperatureValues[index] = (float) temperature;
+            pondusHydrogeniiValues[index] = (float) ph;
+            turbidityValues[index] = (float) turbidity;
+            timestamps[index] = formattedDate;
+
+            // Update chart data with the new LineDataSets
+
+            // Reset index to 0 when it reaches the array length
+            index = (index + 1) % temperatureValues.length;
+            updateCharts();
+
+        } else {
+            Log.e("TAG", "Index out of bounds: " + index);
+        }
+    }
+
+
+
+
+     private List<Entry> getEntries(float[] values, int index) {
         List<Entry> entries = new ArrayList<>();
-        for (int i = 0; i < index; i++) {
-            entries.add(new Entry(i, values[i]));
+        int startIndex = Math.max(0, index - 5);
+        for (int i = startIndex; i < index; i++) {
+            entries.add(new Entry(i - startIndex, values[i]));
         }
         return entries;
     }
+
 }
